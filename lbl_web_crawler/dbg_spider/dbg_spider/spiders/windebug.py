63d7196837390cc5011283a37ac3bed06b281398
tashia <shilei@hybridforge.com>
2015-04-10 23:54:16 -0700

    first lame one

--- lbl_web_crawler/dbg_spider/dbg_spider/spiders/windebug.py
+++ lbl_web_crawler/dbg_spider/dbg_spider/spiders/windebug.py
@@ -1,28 +1,35 @@
 from scrapy.spider import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+# LinkExtractor is as same as LxmlLinkExtractor 
+from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.selector import Selector
 
 from dbg_spider.items import DbgSpiderItem
 
 
-class windebug(Spider):
+class windebug(CrawlSpider):
     """description of class"""
 
     name = 'windebug'
-    #allowed_domains = ['blogs.msdn.com']
+    allowed_domains = ['blogs.msdn.com']
     start_urls = ['http://blogs.msdn.com/b/ntdebugging/']
 
-    def parse(self, response):
+    rules = [ 
+        Rule(LinkExtractor('/b/ntdebugging/archive/[^/][0-9/]+\.aspx$'), follow=True ),
+        Rule(LinkExtractor('/b/ntdebugging/archive/[^/][0-9/]+[^/][A-Za-z0-9-]+\.aspx$'), callback='parse_article'),
+        ]
+
+    def parse_article(self, response):
         '''
         '''
         sel = Selector(response)
-        posts = sel.xpath('//div[@class="full-post"]/')
-        items = []
-        for post in posts:
-            item = DbgSpiderItem()
-            contents = post.xpath('div[@class="post-content"]/p')
-            for content in contents:# if content.xpath('text()').extract():
-                item['name'] = post.xpath('h3/text()').extract()
-                item['description'] = content.xpath('text()').extract()
-                item['url'] = response.url
-                items.append(item)
-        return items
+        item = DbgSpiderItem()
+        item['name'] = response.xpath('//title/text()').extract()
+        item['url'] = response.url
+        contents = response.xpath('//p')
+        description = ''
+        # to filter out content
+        for content in contents:# if content.xpath('text()').extract():
+            description += str(content.xpath('text()').extract())
+        item['description'] = description
+        return item
